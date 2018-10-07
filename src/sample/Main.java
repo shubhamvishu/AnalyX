48e6723c76471c4a56d1caed1013cc46f16a1ea9
Shubham <shubhamvishu171@gmail.com>
2018-10-07 13:57:22 +0530

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    Stage primaryStage=new Stage();
+
+    @Override
+    public void start(Stage primary) throws Exception{
+        try {
+            //System.out.println("11");
+            Parent root = (Parent)FXMLLoader.load(getClass().getResource("log.fxml"));
+            System.out.println("shubham");
+            primaryStage.setTitle("Galleria Mall");
+            Scene scene=new Scene(root,701, 461);
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            //System.out.println("22");
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
