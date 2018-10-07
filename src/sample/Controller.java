afe0ff9f4e8f49f1a46a13a3263fdeb9ffb37fee
Shubham <shubhamvishu171@gmail.com>
2018-10-07 16:20:10 +0530

    Modified the login screen...added two tabs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,48 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    AnchorPane in;
+    @FXML
+    AnchorPane about;
+    @FXML
+    AnchorPane mainlog;
+    @FXML
+    AnchorPane mainabout;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        in.setVisible(true);
+
+    }
+    @FXML
+    void logg(MouseEvent event)
+    {
+        //System.out.println("shubham123"+event.getSource()+" "+in+" "+about);
+        if(event.getSource()==in) {
+            in.setStyle("-fx-background-color:#00ff00;-fx-border-color:#fff;-fx-border-width:5px;");
+            about.setStyle("-fx-background-color: #B2BABB;");
+            mainlog.setVisible(true);
+            mainabout.setVisible(false);
+        }
+        else if(event.getSource()==about){
+            System.out.println("B");
+            about.setStyle("-fx-background-color: #B2BABB;-fx-border-color:#fff;-fx-border-width:5px;");
+            in.setStyle("-fx-background-color:#00ff00;");
+            mainlog.setVisible(false);
+            mainabout.setVisible(true);
+
+        }
+    }
 }
